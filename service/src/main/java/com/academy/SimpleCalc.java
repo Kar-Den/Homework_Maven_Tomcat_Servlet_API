ec1b46e48de9551d8a4cfc4021f081872d7fbd83
Kar-Den <k.denis.vlad@gmail.com>
2020-08-08 18:29:06 +0300

    создал клас калькулятор

---
+++ service/src/main/java/com/academy/SimpleCalc.java
@@ -0,0 +1,17 @@
+package com.academy;
+
+public class SimpleCalc {
+    public double sum (double a, double b){
+        return a + b;
+    }
+    public double minus (double a, double b){
+        return a - b;
+    }
+    public double mul (double a, double b){
+        return a * b;
+    }
+    public double div (double a, double b){
+        return a / b;
+    }
+
+}
