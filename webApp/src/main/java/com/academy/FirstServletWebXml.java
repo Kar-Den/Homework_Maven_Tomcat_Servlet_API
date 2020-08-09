c528f3b89be53f034ad3417819c1226253a9ed87
Kar-Den <k.denis.vlad@gmail.com>
2020-08-09 23:33:45 +0300

    оба сервлета запускаются

--- webApp/src/main/java/com/academy/FirstServletWebXml.java
+++ webApp/src/main/java/com/academy/FirstServletWebXml.java
@@ -7,11 +7,12 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class FirstServletWebXml extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-    }
+//    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//
+//    }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.getWriter().println("<h1>First_Servlet_Web.xml</h1>");
 
     }
 }
