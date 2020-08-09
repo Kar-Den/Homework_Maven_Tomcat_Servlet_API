8f5289e971d4f70fa2d605c727c23eaa60753496
Kar-Den <k.denis.vlad@gmail.com>
2020-08-10 00:36:20 +0300

    сделал методы static для удобства вызова в сервлетах

--- service/src/main/java/com/academy/SimpleCalc.java
+++ service/src/main/java/com/academy/SimpleCalc.java
@@ -1,16 +1,16 @@
 package com.academy;
 
 public class SimpleCalc {
-    public double sum (double a, double b){
+    public static double sum (double a, double b){
         return a + b;
     }
-    public double minus (double a, double b){
+    public static double minus (double a, double b){
         return a - b;
     }
-    public double mul (double a, double b){
+    public static double mul (double a, double b){
         return a * b;
     }
-    public double div (double a, double b){
+    public static double div (double a, double b){
         return a / b;
     }
 
