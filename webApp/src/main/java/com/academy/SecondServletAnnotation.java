aec698f576ca5b9d6e863d033b7629f4a28f5a5b
Kar-Den <k.denis.vlad@gmail.com>
2020-08-10 00:22:02 +0300

    меняю url-pattern

--- webApp/src/main/java/com/academy/SecondServletAnnotation.java
+++ webApp/src/main/java/com/academy/SecondServletAnnotation.java
@@ -7,13 +7,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "SecondServletAnnotation", urlPatterns = "/Servlet2")
+@WebServlet(name = "SecondServletAnnotation", urlPatterns = "/secondServlet")
 public class SecondServletAnnotation extends HttpServlet {
 //    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 //
 //    }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.getWriter().println("<h1>SecondServletAnnotation</h1>");
+        response.getWriter().println("<h1>Second-Servlet-Annotation</h1>");
     }
 }
