5dd93abb0b84c8a9c4232a82d11f68373998265f
Kar-Den <k.denis.vlad@gmail.com>
2020-08-10 11:47:42 +0300

    во второй сервлет в метод doPost вставил код калькулятора для расчёта значений, полученнных при запросе POST через форму.

--- webApp/src/main/java/com/academy/SecondServletAnnotation.java
+++ webApp/src/main/java/com/academy/SecondServletAnnotation.java
@@ -9,9 +9,34 @@ import java.io.IOException;
 
 @WebServlet(name = "SecondServletAnnotation", urlPatterns = "/secondServlet")
 public class SecondServletAnnotation extends HttpServlet {
-//    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//
-//    }
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String a = request.getParameter("firstNum");
+        String b = request.getParameter("secondNum");
+        String operation = request.getParameter("operation");
+
+        Double aa = new Double(a);
+        Double bb = new Double(b);
+
+        switch (operation) {
+            case "plus":
+                response.getWriter().println(a + " + " + b + " = " + SimpleCalc.sum(aa, bb));
+                break;
+            case "minus":
+                response.getWriter().println(a + " - " + b + " = " + SimpleCalc.minus(aa, bb));
+                break;
+            case "mul":
+                response.getWriter().println(a + " * " + b + " = " + SimpleCalc.mul(aa, bb));
+                break;
+            case "div":
+                response.getWriter().println(a + " : " + b + " = " + SimpleCalc.div(aa, bb));
+                break;
+            default:response.getWriter().println("Look to operation");
+
+        }
+
+
+    }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.getWriter().println("<h1>Second-Servlet-Annotation</h1>");
